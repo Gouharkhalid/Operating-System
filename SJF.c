7cbd30d726f9611ce09cda68dc57c4099ffe0a2f
Gouharkhalid <112921120+Gouharkhalid@users.noreply.github.com>
2025-09-10 18:14:36 +0530

    Create SJF.c
    Shortest Job First algorithm for process scheduling.

---
+++ SJF.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct pro 
+{
+    int pid;
+    int rt;
+    int ct;
+    int at;
+    int bt;
+    int tat;
+};
+void sch(struct pro* p[],int len)
+{
+    int t=0;
+    struct pro * temp;
+    for (int i=0;i<len;i++)
+    {
+        temp=p[0];
+        int f=0;
+        for (int j=0;j<len;j++)
+        {
+            if (temp->bt ==-1 && p[j]->bt !=-1)
+            {
+                temp=p[j];
+            }
+            if (p[j]->bt!=-1 && p[j]->at<=t && temp->bt>p[j]->bt)
+            {
+                temp=p[j];
+                f=1;
+            }
+        }
+        if (f==0)
+        {
+            for (int k=0;k<len;k++)
+            {
+                if(temp->at>t && p[k]->bt!=-1 && temp->at>p[k]->at)
+                {
+                    temp=p[k];
+                }             
+            }
+            t=temp->at;
+        }
+        t=t+temp->bt;
+        temp->ct=t;
+        temp->bt=-1;
+        temp->tat=temp->ct-temp->at;
+    }
+}
+int main ()
+{
+    int n;
+    printf("enter the no of process");
+    scanf("%d",&n);
+    struct pro* a[n];
+    for (int j=0;j<n;j++)
+    {
+        a[j]=(struct pro*)malloc(sizeof(struct pro));
+        printf("enter the process id");
+        scanf("%d",&a[j]->pid);
+        printf("enter the arrival time of the process");
+        scanf("%d",&a[j]->at);
+        printf("enter the bust time of the process");
+        scanf("%d",&a[j]->bt);
+        a[j]->tat=0;
+        a[j]->ct=0;
+        a[j]->rt=0;
+    }
+    sch(a,n);
+    int ct=0;
+    int tat=0;
+    for (int i=0;i<n;i++)
+    {
+        printf("%d    |%d   |",a[i]->tat,a[i]->ct);
+        ct+=a[i]->ct;
+        tat+=a[i]->tat;
+    }
+    printf("the average completion time is :%.3f",ct/n);
+    printf("the average turn around time is :%.3f",tat/n);
+    return 0;
+}
